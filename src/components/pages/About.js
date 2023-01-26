ac6486013a40c691e7b462e1bec52a11f9f4b4c7
amiller0806 <ariel.miller555@gmail.com>
2023-01-26 16:24:37 -0500

    fixed about

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,53 +1,17 @@
 import React from 'react';
 
-// Here we are using object destructuring assignment to pluck off our variables from the props object
-// We assign them to their own variable names
-function NavTabs({ currentPage, handlePageChange }) {
+export default function About() {
   return (
-    <ul className="nav nav-tabs">
-      <li className="nav-item">
-        <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
-          // This is a conditional (ternary) operator that checks to see if the current page is "Home"
-          // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
-        >
-          Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#blog"
-          onClick={() => handlePageChange('Blog')}
-          // Check to see if the currentPage is `Blog`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Blog' ? 'nav-link active' : 'nav-link'}
-        >
-          Blog
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#contact"
-          onClick={() => handlePageChange('Contact')}
-          // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-        >
-          Contact
-        </a>
-      </li>
-    </ul>
+    <div>
+      <h1>About Page</h1>
+      <p>
+        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
+        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
+        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
+        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
+        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
+        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+      </p>
+    </div>
   );
 }
-
-export default NavTabs;
