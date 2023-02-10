ce767aa726a99c8c9516d79014509af7ec17255f
amiller0806 <ariel.miller555@gmail.com>
2023-02-10 17:34:20 -0500

    finished styling about section and resume

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,21 +1,21 @@
 import React from 'react';
+import profilePic from '../../assets/images/ariel-miller.jpeg';
+
 
 export default function About() {
     return (
-
-        <main id="contact" className="page-content container">
-                  <h2 className="page-heading">About Me</h2>
-    <div>
-     
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-            </div>
-            </main>
-  );
+      <main id="contact" className="page-content container">
+        <h2 className="page-heading">About Me</h2>
+        <div>
+            <img className="profile" src={profilePic} alt="woman in black dress smiling"/>
+        </div>
+        <div className= "about-container">
+          <p className = "about-text">
+            Ariel Miller is a full stack web developer with a strong proficiency in HTML, CSS,
+            JS, REACT, MERN stack, Sequelize, MySQL, NoSQL, MongoDB, Mongoose,
+            Bootstrap, and MaterializeCSS.
+          </p>
+        </div>
+      </main>
+    );
 }
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -7,9 +7,13 @@ export default function Resume() {
       <main id="resume" className="page-content container">
         <h2 className="page-heading"> Resume </h2>
         <div>
+        <h4 className = "resume-text">Click on the image of my Resume below to see the full PDF copy!</h4>
+        </div>
+        <div>
           <a href={resumePdf} target="_blank" rel="noreferrer">
             <img
               className="resume"
+              alt = "resume pdf"
               variant="top"
               src={resume}
               height="200px"
