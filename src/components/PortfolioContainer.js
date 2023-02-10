1b4a5d6f2121fe771e587e7379323f2d7012617d
amiller0806 <ariel.miller555@gmail.com>
2023-02-09 23:25:23 -0500

    fixed nav tabs

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -44,10 +44,10 @@ export default function PortfolioContainer() {
       {" "}
       {/* We are passing the currentPage from state and the function to update it */}
       <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
-
+      <main>
         {/* Here we are calling the renderPage method which will return a component  */}
         {renderPage()}
-     
+      </main>
       <Footer />
     </div>
   );
